3e82549cbd8b3a92587971253a25557293d31108
Geovane Araújo <geovane-araujo@outlook.com>
2023-03-05 10:52:56 -0300

    first commit

---
+++ microservice-product/src/main/java/com/microservice/product/microserviceproduct/config/connection/TenantContext.java
@@ -0,0 +1,8 @@
+package com.microservice.product.microserviceproduct.config.connection;
+
+public class TenantContext {
+
+    private static ThreadLocal<String> currentTenant = new ThreadLocal<>();
+    
+
+}
